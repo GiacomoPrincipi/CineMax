c1b62685b2ed93b794e3287a8173bacc629c6cfc
Al3xx4nderr <alessandrops2003@gmail.com>
2025-07-11 15:31:40 +0100

    Update Spettacolo.py

--- Sistema/Spettacolo.py
+++ Sistema/Spettacolo.py
@@ -1,7 +1,5 @@
 class Spettacolo():
-
     def __init__(self, titolo, genere, sala, data, orarioInizio, orarioFine, durata):
-
         self.titolo = titolo
         self.genere = genere
         self.sala = sala
@@ -10,48 +8,31 @@ class Spettacolo():
         self.orarioFine = orarioFine
         self.durata = durata
 
-
     def getInfoSpettacolo(self):
-
         return {self.titolo, self.genere, self.sala, self.data, self.orarioInizio, self.orarioFine, self.durata}
-    
 
     def getTitolo(self):
-
         return self.titolo
     
-
     def getGenere(self):
-
         return self.genere
     
-
     def getSala(self):
-
         return self.sala
     
-
     def getData(self):
-
         return self.data
     
-
     def getOrarioInizio(self):
-
         return self.orarioInizio
     
     def gerOrarioFine(self):
-
         return self.orarioFine
     
-
     def getDurata(self):
-
         return self.durata
     
-
     def setInfoSpettacolo(self, titolo, genere, sala, data, orarioInizio, orarioFine, durata):
-
        self.setTitolo(self, titolo)
        self.setGenere(self, genere)
        self.setSala(self, sala)
@@ -60,37 +41,23 @@ class Spettacolo():
        self.setOrarioFine(self, orarioFine)
        self.setDurata(self, durata)
 
-
     def setTitolo(self, titolo):
-
         self.titolo = titolo
 
-
     def setGenere(self, genere):
-
         self.genere = genere
-
     
     def setSala(self, sala):
-
         self.sala = sala
 
-
     def setData(self, data):
-
         self.data = data
 
-
     def setOrarioInizio(self, orarioInizio):
-
         self.orarioInizio = orarioInizio
 
-
     def setOrarioFine(self, orarioFine):
-
         self.orarioFine = orarioFine
 
-
     def setDurata(self, durata):
-
         self.durata = durata
