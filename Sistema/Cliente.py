5e0d65b15b19654d31d271c994e86a2446e99a01
Al3xx4nderr <alessandrops2003@gmail.com>
2025-07-11 15:46:10 +0100

    Update Cliente.py

--- Sistema/Cliente.py
+++ Sistema/Cliente.py
@@ -1,1 +1,23 @@
+class Cliente():
+    def __init__(self, codiceFiscale, punti):
+        self.codiceFiscale = codiceFiscale
+        self.punti = punti
+        
+    def getInfoCliente(self):
+        return {self.cliente, self.data, self.ora, self.articolo, self.tipo, self.importo}
 
+    def getCodiceFiscale(self):
+        return self.codiceFiscale
+    
+    def getPunti(self):
+        return self.punti
+    
+    def setInfoCliente(self, codiceFiscale, punti):
+       self.codiceFiscale(self, codiceFiscale)
+       self.punti(self, punti)
+
+    def setCodiceFiscale(self, codiceFiscale: string):
+        self.codiceFiscale = codiceFiscale
+
+    def setPunti(self, punti: int):
+        self.punti = punti
