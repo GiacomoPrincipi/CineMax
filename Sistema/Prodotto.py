dec04061b3049e8317ce8dbd6225239435ded21f
Giacomo Principi <97121649+GiacomoPrincipi@users.noreply.github.com>
2025-07-11 18:18:32 +0200

    Update Prodotto.py

--- Sistema/Prodotto.py
+++ Sistema/Prodotto.py
@@ -1,12 +1,19 @@
-class Prodotto():
-    def __init__(self, nome, ingredienti, allergeni):
+class Prodotto(Articolo):
+    def __init__(self, prezzo, disponibile, nome, ingredienti, allergeni):
+        super().__init__(prezzo, disponibile)
         self.nome = nome
         self.ingredienti = ingredienti
         self.allergeni = allergeni
 
     def getInfoProdotto(self):
-        return {self.nome, self.ingredienti, self.allergeni}
+        return {self.prezzo, self.disponibile, self.nome, self.ingredienti, self.allergeni}
 
+     def getPrezzo(self):
+        return self.prezzo
+
+    def getDisponibile(self):
+        return self.disponibile
+    
     def getNome(self):
         return self.nome
 
@@ -16,16 +23,24 @@ class Prodotto():
     def getAllergeni(self):
         return self.allergeni
 
-    def setInfoProdotto(self, nome, ingredienti, allergeni):
+    def setInfoProdotto(self, prezzo, disponibile, nome, ingredienti, allergeni):
+        self.setPrezzo(self, prezzo)
+        self.setDisponibile(self, disponibile)
         self.setNome(self, nome)
         self.setIngredienti(self, ingredienti)
         self.setAllergeni(self, allergeni)
 
-    def setNome(self, nome: string):
-        self.Spettacolo = spettacolo
+    def setPrezzo(self, prezzo):
+        self.prezzo = prezzo
 
-    def setIngredienti(self, ingredienti: ArrayList<string>):
+    def setDisponibile(self, disponibile):
+        self.disponibile = disponibile
+
+    def setNome(self, nome):
+        self.nome = nome
+
+    def setIngredienti(self, ingredienti):
         self.ingredienti = ingredienti
 
-    def setAllergeni(self, allergeni: ArrayList<string>):
-        self.posto = posto
+    def setAllergeni(self, allergeni):
+        self.allergeni = allergeni
