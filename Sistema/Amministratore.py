f0cd2ba0e92a6cbf1c6be1964e6d77e08dc4728e
Al3xx4nderr <alessandrops2003@gmail.com>
2025-07-11 15:59:48 +0100

    Update Amministratore.py

--- Sistema/Amministratore.py
+++ Sistema/Amministratore.py
@@ -1,1 +1,22 @@
+class Amministratore():
+    def __init__(self, matricola):
+        self.matricola = matricola
 
+    def getInfoAmministratore(self):
+        return {self.matricola}
+
+    def getMatricola(self):
+        return self.matricola
+
+    def getDisponibile(self):
+        return self.disponibile
+      
+    def setInfoArticolo(self, prezzo, disponibile):
+        self.setPrezzo(self,Prezzo)
+        self.setDisponibile(self,disponibile)
+
+    def setPrezzo(self, prezzo: float):
+        self.prezzo = prezzo
+
+    def setTipo(self, disponibile: bool):
+        self.disponibile = disponibile
