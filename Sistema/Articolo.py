28f49eaeac983d5b5e8a5312c53f3619bfec7814
Al3xx4nderr <alessandrops2003@gmail.com>
2025-07-11 15:17:03 +0100

    Update Articolo.py

--- Sistema/Articolo.py
+++ Sistema/Articolo.py
@@ -1,1 +1,23 @@
+class Articolo:
+    def __init__(self,prezzo,disponibile):
+        self.prezzo = prezzo
+        self.disponibile = disponibile
 
+    def getInfoArticolo(self):
+        return {self.prezzo, self.disponibile}
+
+    def getPrezzo(self):
+        return self.prezzo
+
+    def getDisponibile(self):
+        return self.disponibile
+      
+    def setInfoArticolo(self, prezzo, disponibile):
+        self.setPrezzo(self,Prezzo)
+        self.setDisponibile(self,disponibile)
+
+    def setPrezzo(self, prezzo: float):
+        self.prezzo = prezzo
+
+    def setTipo(self, disponibile: bool):
+        self.disponibile = disponibile
