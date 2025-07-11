ac9d0c979cbf292496f97db9c373b5184db57fc8
Al3xx4nderr <alessandrops2003@gmail.com>
2025-07-11 15:51:22 +0100

    Update Prodotto.py

--- Sistema/Prodotto.py
+++ Sistema/Prodotto.py
@@ -1,1 +1,31 @@
+class Prodotto():
+    def __init__(self, nome, ingredienti, allergeni):
+        self.nome = nome
+        self.ingredienti = ingredienti
+        self.allergeni = allergeni
 
+    def getInfoProdotto(self):
+        return {self.nome, self.ingredienti, self.allergeni}
+
+    def getNome(self):
+        return self.nome
+
+    def getIngredienti(self):
+        return self.ingredienti
+
+    def getAllergeni(self):
+        return self.allergeni
+
+    def setInfoProdotto(self, nome, ingredienti, allergeni):
+        self.setNome(self, nome)
+        self.setIngredienti(self, ingredienti)
+        self.setAllergeni(self, allergeni)
+
+    def setNome(self, nome: string):
+        self.Spettacolo = spettacolo
+
+    def setIngredienti(self, ingredienti: ArrayList<string>):
+        self.ingredienti = ingredienti
+
+    def setAllergeni(self, allergeni: ArrayList<string>):
+        self.posto = posto
