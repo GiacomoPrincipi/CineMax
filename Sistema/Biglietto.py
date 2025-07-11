3f4534e8d8cfc466989efbf3c6bda1455908e9c8
Al3xx4nderr <alessandrops2003@gmail.com>
2025-07-11 12:23:03 +0200

    Update Biglietto.py

--- Sistema/Biglietto.py
+++ Sistema/Biglietto.py
@@ -1,1 +1,36 @@
+class Biglietto:
+    def __init__(self,spettacolo,tipo,posto):
+        self.Spettacolo = spettacolo
+        self.tipo = tipo
+        self.posto = posto
+        
+    def create(self, prezzo: float, ridotto: bool, spettacolo: Spettacolo, tipo: str, posto: int):
+        self.spettacolo = spettacolo
+        self.tipo = tipo
+        self.posto = posto
 
+    def getInfoBiglietto(self):
+        return {self.Spettacolo, self.tipo, self.posto}
+
+    def getSpettacolo(self):
+        return self.spettacolo
+
+    def getTipo(self):
+        return self.tipo
+
+    def getPosto(self):
+        return self.posto
+
+    def setInfoBiglietto(self, spettacolo, tipo, posto):
+        self.setSpettacolo(self,spettacolo)
+        self.setTipo(self,tipo)
+        self.setPosto(self,posto)
+
+    def setSpettacolo(self, spettacolo: Spettacolo):
+        self.spettacolo = spettacolo
+
+    def setTipo(self, tipo: str):
+        self.tipo = tipo
+
+    def setPosto(self, posto: int):
+        self.posto = posto
