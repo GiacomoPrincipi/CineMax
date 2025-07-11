8592aba68cbca0c7aff65f340ccce02b2a58c55c
Giacomo Principi <97121649+GiacomoPrincipi@users.noreply.github.com>
2025-07-11 18:09:40 +0200

    Update Amministratore.py

--- Sistema/Amministratore.py
+++ Sistema/Amministratore.py
@@ -1,15 +1,58 @@
-class Amministratore():
+class Amministratore(Utente):
     def __init__(self, matricola):
+        super().__init__(nome, cognome, dataNascita, email, telefono, password)
         self.matricola = matricola
 
     def getInfoAmministratore(self):
-        return {self.matricola}
+        return {self.nome, self.cognome, self.dataNascita, self.email, self.telefono, self.password, self.matricola}
 
+    def getNome(self):
+        return self.nome
+    
+    def getCognome(self):
+        return self.cognome
+    
+    def getDataNascita(self):
+        return self.dataNascita
+    
+    def getEmail(self):
+        return self.email
+    
+    def getTelefono(self):
+        return self.telefono
+    
+    def getPassword(self):
+        return self.password
+    
     def getMatricola(self):
         return self.matricola
       
-    def setInfoAmministratore(self, matricola):
+    def setInfoAmministratore(self, nome, cognome, dataNascita, email, telefono, password, matricola):
+       self.setNome(self, nome)
+       self.setCognome(self, cognome)
+       self.setDataNascita(self, dataNascita)
+       self.setEmail(self, email)
+       self.setTelefono(self, telefono)
+       self.setPassword(self, password)
         self.setMatricola(self, matricola)
-        
-    def setMatricola(self, matricola: string):
+
+    def setNome(self, nome):
+        self.nome = nome
+
+    def setCognome(self, cognome):
+        self.cognome = cognome
+    
+    def setDataNascita(self, dataNascita):
+        self.dataNascita = dataNascita
+
+    def setEmail(self, email):
+        self.email = email
+
+    def setTelefono(self, telefono):
+        self.telefono = telefono
+
+    def setPassword(self, password):
+        self.password = password
+    
+    def setMatricola(self, matricola):
         self.matricola = matricola
