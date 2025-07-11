897002a15fe9a9c5639bb9bf1609534687cde66a
Al3xx4nderr <alessandrops2003@gmail.com>
2025-07-11 15:41:26 +0100

    Update Recensione.py

--- Sistema/Recensione.py
+++ Sistema/Recensione.py
@@ -1,1 +1,47 @@
+class Recensione():
+    def __init__(self, cliente, data, ora, stelle, testo):
+        self.cliente = cliente
+        self.data = data
+        self.ora = ora
+        self.articolo = stelle
+        self.tipo = testo
+      
+    def getInfoRecensione(self):
+        return {self.cliente, self.data, self.ora, self.stelle, self.testo}
 
+    def getCliente(self):
+        return self.cliente
+    
+    def getData(self):
+        return self.data
+    
+    def getOra(self):
+        return self.ora
+
+    def getStelle(self):
+        return self.stelle
+    
+    def getTesto(self):
+        return self.testo
+    
+    def setInfoRecensione(self, cliente, data, ora, stelle, testo):
+       self.setCliente(self, cliente)
+       self.setData(self, data)
+       self.setOra(self, ora)
+       self.setStelle(self, stelle)
+       self.setTesto(self, testo)
+
+    def setCliente(self, cliente: Cliente):
+        self.cliente = cliente
+
+    def setData(self, data: Data):
+        self.data = data
+
+    def setOra(self, ora: Ora):
+        self.ora = ora
+
+    def setStelle(self, stelle: int):
+        self.articolo = articolo
+
+    def setTesto(self, testo: string):
+        self.testo = testo
