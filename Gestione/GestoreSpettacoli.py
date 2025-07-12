168030355acb140bce33cea6fe1f79224ad193f7
Giacomo Principi <97121649+GiacomoPrincipi@users.noreply.github.com>
2025-07-12 14:48:57 +0200

    Update GestoreSpettacoli.py

--- Gestione/GestoreSpettacoli.py
+++ Gestione/GestoreSpettacoli.py
@@ -1,1 +1,35 @@
+import os.path
+import pickle
+
+class GestoreSpettacoli():
+
+    def __init__(self, listaSpettacoli):
+        self.listaSpettacoli = listaSpettacoli
+
+    def caricaDatiSpettacoli(self):
+        listaDatiSpettacoli = []
+        if os.path.isfile('Dati/DatiSpettacoli.pickle'):
+            with open('Dati/DatiSpettacoli.pickle', 'rb') as file:
+                listaDatiSpettacoli = pickle.load(file)
+        return listaDatiSpettacoli
+
+    def salvaDatiSpettacoli(self, listaSpettacoli):
+        with open('Dati/DatiSpettacoli.pickle', 'wb') as file:
+            pickle.dump(listaSpettacoli, file)
+
+    def getListaSpettacoli(self):
+        listaSpettacoli = self.caricaDatiSpettacoli()
+        return listaSpettacoli
+    
+    def inserisciSpettacolo(self, listaSpettacoli, spettacolo):
+        listaSpettacoli = self.caricaDatiSpettacoli()
+        listaSpettacoli.append(spettacolo)
+        self.salvaDatiSpettacoli(listaSpettacoli)
+    
+
+    def rimuoviSpettacolo(self, listaSpettacoli, spettacolo):
+        listaSpettacoli = self.caricaDatiSpettacoli()
+        listaSpettacoli.remove(spettacolo)
+        self.salvaDatiSpettacoli(listaSpettacoli)
+        del spettacolo
 
