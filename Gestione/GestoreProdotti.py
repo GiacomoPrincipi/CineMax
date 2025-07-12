d961621ec2b3fa71feb0566c15b40ced769e8023
Giacomo Principi <97121649+GiacomoPrincipi@users.noreply.github.com>
2025-07-12 14:00:27 +0200

    Update GestoreProdotti.py

--- Gestione/GestoreProdotti.py
+++ Gestione/GestoreProdotti.py
@@ -1,1 +1,40 @@
+import os.path
+import pickle
 
+class GestoreProdotti():
+
+    def __init__(self, listaProdotti):
+        self.listaProdotti = listaProdotti
+
+    def caricaDatiProdotti(self):
+        listaDatiProdotti = []
+        if os.path.isfile('Dati/DatiProdotti.pickle'):
+            with open('Dati/DatiProdotti.pickle', 'rb') as file:
+                listaDatiProdotti = pickle.load(file)
+        return listaDatiProdotti
+
+    def salvaDatiProdotti(self, listaProdotti):
+        with open('Dati/DatiProdotti.pickle', 'wb') as file:
+            pickle.dump(listaProdotti, file)
+
+    def getListaProdotti(self):
+        listaProdotti = self.caricaDatiProdotti()
+        return listaProdotti
+    
+    def inserisciProdottto(self, listaProdotti, prodotto):
+        listaProdotti = self.caricaDatiProdotti()
+        listaProdotti.append(prodotto)
+        self.salvaDatiProdotti(listaProdotti)
+    
+
+    def rimuoviProdotto(self, listaProdotti, prodotto):
+        listaProdotti = self.caricaDatiProdotti()
+        listaProdotti.remove(prodotto)
+        self.salvaDatiProdotti(listaProdotti)
+        del prodotto
+
+    def controlloDisponibilità(self, prodottoDaControllare):
+        listaProdotti = self.caricaDatiProdotti()
+        for prodotto in listaProdotti:
+            if prodotto == prodottoDaControllare:
+                return prodotto.disponibile
