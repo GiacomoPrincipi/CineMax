475234dae9c718ab57880f461bc1b550459345b3
Giacomo Principi <97121649+GiacomoPrincipi@users.noreply.github.com>
2025-07-12 14:49:29 +0200

    Update GestorePagamenti.py

--- Gestione/GestorePagamenti.py
+++ Gestione/GestorePagamenti.py
@@ -1,1 +1,36 @@
+import os.path
+import pickle
+
+class GestorePagamenti():
+
+    def __init__(self, listaPagamenti):
+        self.listaPagamenti = listaPagamenti
+
+    def caricaDatiPagamenti(self):
+        listaDatiPagamenti = []
+        if os.path.isfile('Dati/DatiPagamenti.pickle'):
+            with open('Dati/DatiPagamenti.pickle', 'rb') as file:
+                listaDatiPagamenti = pickle.load(file)
+        return listaDatiPagamenti
+
+    def salvaDatiPagamenti(self, listaPagamenti):
+        with open('Dati/DatiPagamenti.pickle', 'wb') as file:
+            pickle.dump(listaPagamenti, file)
+
+    def getListaPagamenti(self):
+        listaPagamenti = self.caricaDatiPagamenti()
+        return listaPagamenti
+    
+    def getListaPagamentiCliente(self, cliente):
+        listaPagamenti = self.caricaDatiPagamenti()
+        listaPagamentiCliente = []
+        for pagamento in listaPagamenti:
+            if pagamento.cliente == cliente:
+                listaPagamentiCliente.append(pagamento)
+        return listaPagamentiCliente
+    
+    def inserisciPagamento(self, listaPagamenti, pagamento):
+        listaPagamenti = self.caricaDatiPagamenti()
+        listaPagamenti.append(pagamento)
+        self.salvaDatiPagamenti(listaPagamenti)
 
