2af37c1eabb756669488bf1a3f2348d83d5f84e4
Giacomo Principi <97121649+GiacomoPrincipi@users.noreply.github.com>
2025-07-12 13:59:53 +0200

    Update GestoreBiglietti.py

--- Gestione/GestoreBiglietti.py
+++ Gestione/GestoreBiglietti.py
@@ -1,1 +1,48 @@
+import os.path
+import pickle
 
+class GestoreBiglietti():
+
+    def __init__(self, listaBiglietti):
+        self.listaBiglietti = listaBiglietti
+
+    def caricaDatiBiglietti(self):
+        listaDatiBiglietti = []
+        if os.path.isfile('Dati/DatiBiglietti.pickle'):
+            with open('Dati/DatiBiglietti.pickle', 'rb') as file:
+                listaDatiBiglietti = pickle.load(file)
+        return listaDatiBiglietti
+
+    def salvaDatiBiglietti(self, listaBiglietti):
+        with open('Dati/DatiBiglietti.pickle', 'wb') as file:
+            pickle.dump(listaBiglietti, file)
+
+    def getListaBiglietti(self):
+        listaBiglietti = self.caricaDatiBiglietti()
+        return listaBiglietti
+    
+    def getListaBigliettiSpettacolo(self, spettacolo):
+        listaBiglietti = self.caricaBiglietti()
+        listaBigliettiSpettacolo = []
+        for biglietto in listaBiglietti:
+            if biglietto.spettacolo == spettacolo:
+                listaBigliettiSpettacolo.append(biglietto)
+        return listaBigliettiSpettacolo
+    
+    def inserisciBiglietto(self, listaBiglietti, biglietto):
+        listaBiglietti = self.caricaDatiBiglietti()
+        listaBiglietti.append(biglietto)
+        self.salvaDatiBiglietti(listaBiglietti)
+    
+
+    def rimuoviBiglietto(self, listaBiglietti, biglietto):
+        listaBiglietti = self.caricaDatiBiglietti()
+        listaBiglietti.remove(biglietto)
+        self.salvaDatiBiglietti(listaBiglietti)
+        del biglietto
+
+    def controlloDisponibilità(self, bigliettoDaControllare):
+        listaBiglietti = self.caricaDatiBiglietti()
+        for biglietto in listaBiglietti:
+            if biglietto == bigliettoDaControllare:
+                return biglietto.disponibile
