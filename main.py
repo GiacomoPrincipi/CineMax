932da140d54e9da9999890448ed4e72da26abc2a
Giacomo Principi <97121649+GiacomoPrincipi@users.noreply.github.com>
2025-07-27 17:51:05 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import sys
 
+from PySide6.QtWidgets import QApplication
+from VistaPrincipale import VistaPrincipale
+from Gestione.GestoreAutenticazione import GestoreAutenticazione
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyle('Windows')
+    statoLogin = GestoreAutenticazione()
+    vistaPrincipale = VistaPrincipale(statoLogin)
+    vistaPrincipale.show()
+    sys.exit(app.exec())
